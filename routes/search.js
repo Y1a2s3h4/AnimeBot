04791e88f6a49b80342dffd880ae9240cd405f50
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-06-17 22:06:13 +0530

    tried to resolve promise

--- routes/search.js
+++ routes/search.js
@@ -5,17 +5,17 @@ const search = async (term) => {
   const html = await request(`https://animekayo.com/?s=${term}`);
   const $ = cheerio.load(html);
   const animeList = $("article");
-  const animeArr = await [];
+  const animeArr = [];
   await openBrowser();
   await login();
   animeList.each((i, el) => {
     const drive = fetchDrive($(el).find(".post-title").attr("href"));
-    console.log(drive.then((res) => res));
+    console.log(drive.then((res) => res).catch((err) => err));
     animeArr.push({
       title: $(el).find(".post-title").text().trim(),
       img: $(el).find(".img-holder").attr("data-bsrjs"),
       link: $(el).find(".post-title").attr("href"),
-      // drive: drive.then((res) => res),
+      drive: drive.then((res) => res).catch((err) => err),
     });
   });
   console.log(animeArr);
